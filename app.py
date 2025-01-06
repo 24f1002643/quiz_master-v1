2744abef8643d30982a045faad401387dad22e73
SamarKamboj <samarkamboj98@gmail.com>
2025-01-06 11:39:11 +0530

    added signup page

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, request, session, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 import os
-from datetime import datetime, timezone
+from datetime import datetime
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///database.sqlite3"
@@ -42,7 +42,7 @@ class Quiz(db.Model):
     name = db.Column(db.String(), nullable=False)
     quiz_date = db.Column(db.Date, nullable=False)
     time_duration = db.Column(db.Time, nullable=False)
-    question = db.relationship('Quiz_Question', backref="quiz")
+    question = db.relationship('QuizQuestion', backref="quiz")
     score = db.relationship('Score', backref="quiz")
 
 class Question(db.Model):
@@ -54,7 +54,7 @@ class Question(db.Model):
     option3 = db.Column(db.String(), nullable=False)
     option4 = db.Column(db.String(), nullable=False)
     correct_option = db.Column(db.Integer, nullable=False)
-    quiz = db.relationship("Quiz_Question", backref="question")
+    quiz = db.relationship("QuizQuestion", backref="question")
 
 class QuizQuestion(db.Model):
     quiz_id = db.Column(db.Integer, db.ForeignKey("quiz.id"), primary_key=True, nullable=False)
@@ -74,11 +74,59 @@ with app.app_context():
 
 # Flask work
 @app.route('/', methods=["GET", "POST"])
-def login():
+def index():
     if request.method == "POST":
-        ...
-    return render_template("login.html")
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+        admin = Admin.query.filter_by(username=username, password=password).first()
+        user = User.query.filter_by(username=username, password=password).first()
+        if admin:
+            session["username"] = username
+            session["password"] = password
+            return redirect(url_for("admin"))
+        elif user:
+            session["username"] = username
+            session["password"] = password
+            return redirect(url_for("user", username=username))
+        else:
+            flash("Invalid credentials", 'error')
+            return redirect("/")
+    return render_template("index.html")
 
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        name = request.form.get("name")
+        try:
+            dob = datetime.strptime(request.form.get("dob"), "%Y-%m-%d").date()
+        except ValueError:
+            flash("Enter a valid date of birth", 'error')
+            return redirect(url_for("signup"))
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        # verify and add user credentials then redirect to login
+        # if user's username already exist in admin or user
+        admin = Admin.query.filter_by(username=username).first()
+        user = User.query.filter_by(username=username).first()
+        if admin or user:
+            flash("Please choose a different username!", 'error')
+            return redirect(url_for("signup"))
+
+        db.session.add(User(name=name, dob=dob, username=username, password=password))
+        db.session.commit()
+        flash("Successfully signed up!", 'success')
+        return redirect(url_for("index"))
+    return render_template("signup.html")
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
+
+@app.route("/<username>")
+def user(username):
+    return render_template("user.html", username=username)
 
 if __name__ == "__main__":
     app.run(debug=True)
